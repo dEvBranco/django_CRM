3073acbb23824d3acac5339fd3006e6dcd3c83d2
Eduardo Branco <75887699+dEvBranco@users.noreply.github.com>
2021-02-22 12:05:59 +0000

    views & templtes

--- leads/models.py
+++ leads/models.py
@@ -18,5 +18,5 @@ class Lead(models.Model):
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-def __str__(self):
-    return self.user.email
+    def __str__(self):
+        return self.user.email
