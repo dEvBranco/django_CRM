b149c4307c5ec11fd71ff0a4c6e19d22587f6796
Eduardo Branco <75887699+dEvBranco@users.noreply.github.com>
2021-03-04 14:59:26 +0000

    assigned/unassigned agents

--- leads/forms.py
+++ leads/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UsernameField
-from .models import Lead
+from .models import Lead, Agent
 
 User = get_user_model()
 
@@ -31,4 +31,10 @@ class CustomUserCreationForm(UserCreationForm):
 
 
 class AssignAgentForm(forms.Form):
-    agent = forms.ChoiceField(choices=["agent 1", "agent 2"])
+    agent = forms.ModelChoiceField(queryset=Agent.objects.none())
+    
+    def __init__(self, *args, **kwargs):
+        request = kwargs.pop("request")
+        agents = Agent.objects.filter(organization=request.user.userprofile)
+        super(AssignAgentForm, self).__init__(*args, **kwargs)
+        self.fields["agent"].queryset = agents
--- leads/views.py
+++ leads/views.py
@@ -164,8 +164,22 @@ class AssignAgentView(OrganizerAndLoginRequiredMixin, generic.FormView):
     template_name = "leads/assign_agent.html"
     form_class = AssignAgentForm
     
+    def get_form_kwargs(self, **kwargs):
+        kwargs = super(AssignAgentView, self).get_form_kwargs(**kwargs)
+        kwargs.update({
+            "request": self.request
+        })
+        return kwargs
+    
     def get_success_url(self):
         return reverse("leads:lead-list")
+    
+    def form_valid(self, form):
+        agent = form.cleaned_data["agent"]
+        lead = Lead.objects.get(id=self.kwargs["pk"])
+        lead.agent = agent
+        lead.save()
+        return super(AssignAgentView, self).form_valid(form)
 
 
 # def lead_update(request, pk):
