0b817017a5da87b145291609226d6f2df21b5d41
Eduardo Branco <75887699+dEvBranco@users.noreply.github.com>
2021-03-04 16:02:14 +0000

    category list view

--- leads/views.py
+++ leads/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.views import generic
-from .models import Lead, Agent
+from .models import Lead, Agent, Category
 from agents.mixins import OrganizerAndLoginRequiredMixin
 from .forms import LeadForm, LeadModelForm, CustomUserCreationForm, AssignAgentForm
 
@@ -181,6 +181,42 @@ class AssignAgentView(OrganizerAndLoginRequiredMixin, generic.FormView):
         lead.save()
         return super(AssignAgentView, self).form_valid(form)
 
+
+class CategoryListView(LoginRequiredMixin, generic.ListView):
+    template_name= "leads/category_list.html"
+    context_object_name = "category_list"
+    
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        user = self.request.user
+        
+        if user.is_organizer:
+            queryset = Lead.objects.filter(
+                organization=user.userprofile
+            )
+        else:
+            queryset = Lead.objects.filter(
+                organization=user.agent.organization
+            )
+        
+        context.update({
+            "unassigned_lead_count": queryset.filter(category__isnull=True).count()
+        })
+        return context
+    
+    def get_queryset(self):
+        user = self.request.user
+        # initial queryset of leads for the entire organization
+        if user.is_organizer:
+            queryset = Category.objects.filter(
+                organization=user.userprofile
+            )
+        else:
+            queryset = Category.objects.filter(
+                organization=user.agent.organization
+            )
+        return queryset
+    
 
 # def lead_update(request, pk):
 #     lead = Lead.objects.get(id=pk)
